2de06d729f54a11405d6c1ece5b47d3465332025
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-05-11 12:09:47 -0300

    Criando uma function para a calculadora

---
+++ js/calc.js
@@ -0,0 +1,37 @@
+
+//criando uma função para receber o botao
+
+function insereNumero(botao, visor){
+
+    visor.value += botao.value;
+
+    // console.log(visor);
+}
+
+// function resultado(visor){
+
+    
+//     let novoVisor = visor.value.split("+");
+//     let resultadoOperacao = 0;
+    
+//     novoVisor.forEach((nr) => {
+//         resultadoOperacao += parseInt(nr);
+//     });
+//     console.log(resultadoOperacao);
+
+// }
+
+function resultado(visor){
+
+    let soma = valor.value.split("+");
+    let resultadoOperacao = 0;
+    let subtracao = valor.value.split("-");
+    console.log(subtracao);
+
+    visor.forEach((nr) => {
+        
+        
+    });
+    console.log(resultadoOperacao);
+
+}
